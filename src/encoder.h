f2a053d9ea2571a35f098a36bad46ad46d19b464
svddevelop <svddevelop@gmail.com>
2024-01-01 17:46:44 +0100

    common.h

---
+++ src/encoder.h
@@ -0,0 +1,10 @@
+/* **********************************************
+Author: JackFromBB - jack@boringbar.ru / 
+Placement from: https://github.com/jackfrombb/
+The library for ESP32 under Arduino Environment
+
+In cooperatiion with svdpfaf (svddevelop@gmail.com)
+
+************************************************ */
+#pragma once
+
